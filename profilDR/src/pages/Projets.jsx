6c9702e8f59b16ea5e8ee0bc51731387b9bf8f6c
donovan <donoriviere@gmail.com>
2023-03-22 22:10:45 +0100

    addprojects

--- profilDR/src/pages/Projets.jsx
+++ profilDR/src/pages/Projets.jsx
@@ -4,21 +4,43 @@ import Footer from '../../components/Footer';
 const Projets = () => {
   return (
     <>
-      <section className='projetsSection section'>
+      <section className='section'>
         <h2>Projets</h2>
-        <article className='projetCard'>
-          <h4>Site Autour des jardins de Chéreng</h4>
-          <p>Projet fullstack: React, NodeJS, PostgreSQL</p>
-          <a
-            href='https://autour-des-jardins-de-chereng.netlify.app/'
-            target='_blank'
-          >
-            <img
-              src='./src/assets/siteADJDC.png'
-              alt="page d'accueil du site Autour des jardins de chéreng"
-            />
-          </a>
-        </article>{' '}
+        <div className='projetsSection'>
+          <article className='projetCard'>
+            <h4>Site Autour des jardins de Chéreng</h4>
+            <p>Projet fullstack: React, NodeJS, PostgreSQL</p>
+            <a
+              href='https://autour-des-jardins-de-chereng.netlify.app/'
+              target='_blank'
+            >
+              <img
+                src='./src/assets/siteADJDC.png'
+                alt="page d'accueil du site Autour des jardins de chéreng"
+              />
+            </a>
+          </article>{' '}
+          <article className='projetCard'>
+            <h4>changement de profil</h4>
+            <p>Projet front utilisant l'API randomUser</p>
+            <a href='https://persons-profiles.netlify.app/' target='_blank'>
+              <img src='./src/assets/profiles.png' alt='profils de personnes' />
+            </a>
+            <a href='https://persons-profiles.netlify.app/' target='_blank'>
+              <img src='./src/assets/profil2.png' alt='profils de personnes' />
+            </a>
+          </article>{' '}
+          <article className='projetCard'>
+            <h4>liste de courses</h4>
+            <p>exercice front type todo</p>
+            <a href='https://liste-courses.netlify.app/' target='_blank'>
+              <img src='./src/assets/listeCourses.png' alt='liste de courses' />
+            </a>
+            <a href='https://liste-courses.netlify.app/' target='_blank'>
+              <img src='./src/assets/liste2.png' alt='liste de courses' />
+            </a>
+          </article>{' '}
+        </div>
       </section>
       <Footer />
     </>
